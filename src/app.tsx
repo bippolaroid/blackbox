25d300a02f3e3f506d1bc9aa90ef309ba09f9eee
bippolaroid <bippolaroid@proton.me>
2024-12-13 19:27:25 -0500

    added timer component

--- src/app.tsx
+++ src/app.tsx
@@ -2,19 +2,24 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
+import { Timer } from "./components/Timer";
+import { DateTime } from "luxon";
 import "./app.css";
 
 export default function App() {
   return (
+    <>
     <Router
-      root={props => (
+      root={(props) => (
         <>
           <Nav />
           <Suspense>{props.children}</Suspense>
+          <Timer />
         </>
       )}
     >
       <FileRoutes />
     </Router>
+    </>
   );
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,1 +1,52 @@
-import { createSignal } from "solid-js"
+import { createSignal, onCleanup } from "solid-js";
+import { DateTime } from "luxon";
+
+interface TimerProps {}
+
+const [timer, setTimer] = createSignal(
+  DateTime.fromObject({ hour: 0, minute: 0, second: 0 })
+);
+
+const [timerEnabled, setTimerEnabled] = createSignal(false);
+
+let timerInterval: ReturnType<typeof setInterval> | undefined;
+
+function handleTimer() {
+  if (!timerEnabled()) {
+    setTimerEnabled(true);
+    timerInterval = setInterval(() => {
+      setTimer(timer().plus({ second: 1 })); // Adjust to 10ms for performance
+    }, 1000);
+  } else {
+    if (timerInterval !== undefined) {
+      clearInterval(timerInterval);
+      timerInterval = undefined; // Reset after clearing
+    }
+    setTimerEnabled(false);
+  }
+}
+
+// Cleanup interval on unmount
+onCleanup(() => {
+  if (timerInterval !== undefined) {
+    clearInterval(timerInterval);
+  }
+});
+
+export function Timer(props: TimerProps) {
+  return (
+    <>
+      <div class="bg-neutral-950 px-3 py-6 hover:brightness-105">
+        <h1 class="text-3xl text-neutral-300 font-bold mb-6 cursor-pointer hover:brightness-150">
+          {timer().toFormat("HH:mm:ss")}
+        </h1>
+        <button
+          onClick={handleTimer}
+          class={`px-6 py-3 ${!timerEnabled() ? "bg-green-800" : "bg-red-800"} hover:brightness-150 text-neutral-300 font-bold text-xl transition-colors duration-100`}
+        >
+          {timerEnabled() ? "Stop" : "Start"}
+        </button>
+      </div>
+    </>
+  );
+}
